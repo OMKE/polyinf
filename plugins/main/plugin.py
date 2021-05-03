6b5132f9415cb3c42ccbbfc57dda2385723255dd
Zeljana <zeljanavulovic069@gmail.com>
2021-05-03 13:08:05 +0200

    Updated home plugin
    https://polyinf-singidunum.atlassian.net/browse/PI-34

--- plugins/main/plugin.py
+++ plugins/main/plugin.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from core.plugin.plugin import Plugin
 from core.plugin.plugin_guards import PluginGuards
+from .ui.homepage import Homepage
 
 
 class Main(Plugin):
@@ -16,9 +17,7 @@ class Main(Plugin):
         self.app.log(f'{self.name()} deactivated')
 
     def widget(self, parent=None):
-        widget = QLabel()
-        widget.setAlignment(Qt.AlignCenter)
-        widget.setText('Home plugin')
+        widget = Homepage()
         return widget
 
     def guard(self) -> str:
